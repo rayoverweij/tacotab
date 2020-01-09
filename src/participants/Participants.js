e100e3a1a89258856f0342ffd281d12326e8cfca
Rayo Verweij <rayo.verweij@live.nl>
2020-01-09 16:56:31 +0100

    participant nav

--- src/participants/Participants.js
+++ src/participants/Participants.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import './Participants.scss';
+
+import Tab from 'react-bootstrap/Tab';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
 
 class Participants extends React.Component {
     render() {
         return (
-            <div>
-                <p>Participants in bracket {this.props.bracket}</p>
-            </div>
+            <Tab.Container id={`part-view-${this.props.bracket}`} defaultActiveKey="speakers">
+                <Row className="part-view">
+                    <Col sm={2}>
+                        <Nav variant="pills" className="flex-column">
+                            <Nav.Item>
+                                <Nav.Link eventKey="speakers" className="part-nav-link">Speakers</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="teams" className="part-nav-link">Teams</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="ranking" className="part-nav-link">Ranking</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                    <Col sm={10}>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="speakers">
+                                {this.props.bracket}
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="teams">
+                                O!
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="ranking">
+                                A!
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Col>
+                </Row>
+            </Tab.Container>
         );
     }
 }
