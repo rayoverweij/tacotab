6692b6a6e21c225ba95d9d763f55fb2d9e3dcd00
Rayo Verweij <rayo.verweij@live.nl>
2020-02-08 19:38:05 -0500

    Basic about section

--- src/home/Settings.js
+++ src/home/Settings.js
@@ -36,28 +36,47 @@ class Settings extends React.Component {
         return (
             <div>
                 <Row>
-                    <Col>
-                        <h2>Settings</h2>
-                        <p>Manage your tournament.</p>
+                    <Col md={8}>
+                        <Row>
+                            <Col>
+                                <h2>Settings</h2>
+                                <p>Manage your tournament.</p>
+                            </Col>
+                        </Row>
+                        <Row className="row-settings">
+                            <Col>
+                                <h3>Change tournament name</h3>
+                                <Form onSubmit={this.handleNameFormSubmit}>
+                                    <Form.Row>
+                                        <Col sm={6}>
+                                            <Form.Control
+                                                name="tournament-name"
+                                                type="text"
+                                                placeholder="New name"
+                                                onChange={this.handleNameFormChange} />
+                                        </Col>
+                                        <Col>
+                                            <Button variant="primary" type="submit">Save</Button>
+                                        </Col>
+                                    </Form.Row>
+                                </Form>
+                            </Col>
+                        </Row>
                     </Col>
-                </Row>
-                <Row className="row-settings">
-                    <Col>
-                        <h3>Change tournament name</h3>
-                        <Form onSubmit={this.handleNameFormSubmit}>
-                            <Form.Row>
-                                <Col sm={4}>
-                                    <Form.Control
-                                        name="tournament-name"
-                                        type="text"
-                                        placeholder="New name"
-                                        onChange={this.handleNameFormChange} />
-                                </Col>
-                                <Col>
-                                    <Button variant="primary" type="submit">Save</Button>
-                                </Col>
-                            </Form.Row>
-                        </Form>
+                    <Col md={4}>
+                        <Row>
+                            <Col>
+                                <h3>About</h3>
+                                <p>
+                                    TacoTab alpha version 0.1.0<br />
+                                    <a href="/docs">Documentation</a><br />
+                                    <a href="https://github.com/rayoverweij/tacotab" rel="noopener noreferrer" target="_blank">GitHub</a>
+                                </p>
+                                <p>
+                                    Built by <a href="https://rayo.dev" rel="noopener noreferrer" target="_blank">Rayo Verweij</a>.
+                                </p>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </div>
