5a8df64d8f0710d323c3c67a2d47e4a25c22788a
Rayo Verweij <rayo.verweij@live.nl>
2020-02-17 21:37:35 -0500

    Updates

---
+++ precache-manifest.805f75eb43d49ac1f75b184a0b0f81a1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0963d447db7b1c080797d72df16709",
+    "url": "/tacotab/index.html"
+  },
+  {
+    "revision": "cc2282ee6089531122e7",
+    "url": "/tacotab/static/css/main.b1ea7c75.chunk.css"
+  },
+  {
+    "revision": "2670f4b02e510425fd97",
+    "url": "/tacotab/static/js/2.df9f28ea.chunk.js"
+  },
+  {
+    "revision": "f06e1adbead368d13928b202ef7c07e7",
+    "url": "/tacotab/static/js/2.df9f28ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc2282ee6089531122e7",
+    "url": "/tacotab/static/js/main.4086b0a2.chunk.js"
+  },
+  {
+    "revision": "62f64ff46eccb12c73c9",
+    "url": "/tacotab/static/js/runtime-main.90fdb388.js"
+  },
+  {
+    "revision": "07883e93734b98cae0f7b9c55d287250",
+    "url": "/tacotab/static/media/icon-github.07883e93.svg"
+  },
+  {
+    "revision": "b1b7eac078f3a81bfe82eaee4bc507d6",
+    "url": "/tacotab/static/media/icon-people-filled.b1b7eac0.svg"
+  },
+  {
+    "revision": "85d9f981726a955290e058013a0616d3",
+    "url": "/tacotab/static/media/icon-people.85d9f981.svg"
+  },
+  {
+    "revision": "64bc33927244228f1d0dcab1ca5230ed",
+    "url": "/tacotab/static/media/icon-toggle-off-cyan.64bc3392.svg"
+  },
+  {
+    "revision": "daa1b63de1ad82f1789ccb06874668b8",
+    "url": "/tacotab/static/media/icon-toggle-off.daa1b63d.svg"
+  },
+  {
+    "revision": "a5371628fc0e2b9949910e0047bae436",
+    "url": "/tacotab/static/media/icon-toggle-on-cyan.a5371628.svg"
+  },
+  {
+    "revision": "6f9d06ab732a5f32deb66219bb5b5c9e",
+    "url": "/tacotab/static/media/icon-toggle-on.6f9d06ab.svg"
+  },
+  {
+    "revision": "bdaf1aacb5978d240b967e986ea19044",
+    "url": "/tacotab/static/media/icon-trash-fill.bdaf1aac.svg"
+  },
+  {
+    "revision": "92088904a423491ee7daa3c203101b10",
+    "url": "/tacotab/static/media/icon-trash.92088904.svg"
+  },
+  {
+    "revision": "15863a1a6651536e12ee814becfae8b4",
+    "url": "/tacotab/static/media/logo.15863a1a.svg"
+  }
+]);
