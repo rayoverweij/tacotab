9efa9aa88ce0b72429c5131a524e21831e774a4b
Rayo Verweij <rayo.verweij@live.nl>
2020-05-23 00:20:30 +0200

    Compatibility with exports pre-0.3.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import logo from './images/logo.svg';
-import pgk from '../package.json';
+import pkg from '../package.json';
 import SetupScreen from './setup/SetupScreen';
 import Home from './home/Home';
 import Participants from './participants/Participants';
@@ -47,7 +47,7 @@ class App extends React.Component<AppProps, AppState> {
             localStorage.setItem("tournamentName", s("New tournament"));
         }
         if(!localStorage.getItem("config")) {
-            localStorage.setItem("config", s({version: "0", numDivisions: 0}));
+            localStorage.setItem("config", s({version: pkg.version, numDivisions: 0}));
         }
         if(!localStorage.getItem("speakersOne")) {
             localStorage.setItem("speakersOne", s([]));
@@ -158,7 +158,7 @@ class App extends React.Component<AppProps, AppState> {
             this.updateTournamentName(tournamentName);
             
             let config: Config = {
-                version: pgk.version,
+                version: pkg.version,
                 numDivisions: numDivisions
             }
             if(numDivisions !== 1) {
@@ -175,19 +175,137 @@ class App extends React.Component<AppProps, AppState> {
         const fr = new FileReader();
         fr.onload = event => {
             const result = JSON.parse(event.target!.result as string);
+
             localStorage.setItem("init", s(result.init));
-            localStorage.setItem("tournamentName", s(result.tournamentName));
-            localStorage.setItem("config", s(result.config));
-            localStorage.setItem("speakersOne", s(result.speakersOne));
-            localStorage.setItem("teamsOne", s(result.teamsOne));
-            localStorage.setItem("speakersTwo", s(result.speakersTwo));
-            localStorage.setItem("teamsTwo", s(result.teamsTwo));
-            localStorage.setItem("speakerCounter", s(result.speakerCounter));
-            localStorage.setItem("teamCounter", s(result.teamCounter));
-            localStorage.setItem("judges", s(result.judges));
-            localStorage.setItem("judgeCounter", s(result.judgeCounter));
-            localStorage.setItem("draws", s(result.draws));
-            localStorage.setItem("roomCounter", s(result.roomCounter));
+            // COMPATIBILITY WITH EXPORTS BEFORE VERSION 0.3.0
+            if(!result.config.version) {
+                localStorage.setItem("tournamentName", s(result.tournament_name));
+
+                let importConfig = result.config;
+                importConfig.version = this.state.config.version;
+                if(importConfig.divisions === "1") {
+                    importConfig.numDivisions = 1
+                } else {
+                    importConfig.numDivisions = 2
+                }
+                delete importConfig.divisions
+                localStorage.setItem("config", s(importConfig));
+
+                let importSpeakersOne = result.speakers_one.map((speaker: any) => {
+                    let newSpeaker = {
+                        ...speaker,
+                        speakerID: speaker.debaterID
+                    }
+                    delete newSpeaker.debaterID
+                    return newSpeaker
+                });
+                localStorage.setItem("speakersOne", s(importSpeakersOne));
+
+                let importTeamsOne = result.teams_one.map((team: any) => {
+                    let newTeam = {
+                        ...team,
+                        name: team.teamName,
+                        round1: team.round1.map((sp: string) => parseInt(sp)),
+                        round2: team.round2.map((sp: string) => parseInt(sp)),
+                        round3: team.round2.map((sp: string) => parseInt(sp)),
+                        sideRound1: team.sideR1
+                    }
+                    delete newTeam.teamName
+                    delete newTeam.sideR1
+                    return newTeam
+                });
+                localStorage.setItem("teamsOne", s(importTeamsOne));
+
+                let importSpeakersTwo = result.speakers_two.map((speaker: any) => {
+                    let newSpeaker = {
+                        ...speaker,
+                        speakerID: speaker.debaterID
+                    }
+                    delete newSpeaker.debaterID
+                    return newSpeaker
+                });
+                localStorage.setItem("speakersTwo", s(importSpeakersTwo));
+
+                let importTeamsTwo = result.teams_two.map((team: any) => {
+                    let newTeam = {
+                        ...team,
+                        name: team.teamName,
+                        round1: team.round1.map((sp: string) => parseInt(sp)),
+                        round2: team.round2.map((sp: string) => parseInt(sp)),
+                        round3: team.round2.map((sp: string) => parseInt(sp)),
+                        sideRound1: team.sideR1
+                    }
+                    delete newTeam.teamName
+                    delete newTeam.sideR1
+                    return newTeam
+                });
+                localStorage.setItem("teamsTwo", s(importTeamsTwo));
+
+                localStorage.setItem("speakerCounter", s(result.speakers_counter));
+                localStorage.setItem("teamCounter", s(result.teams_counter));
+
+                let importJudges = result.judges.map((judge: any) => {
+                    let newJudge = {
+                        ...judge,
+                        atRound1: judge.r1,
+                        atRound2: judge.r2,
+                        atRound3: judge.r3
+                    }
+                    delete newJudge.r1
+                    delete newJudge.r2
+                    delete newJudge.r3
+                    return newJudge
+                });
+                localStorage.setItem("judges", s(importJudges));
+
+                localStorage.setItem("judgeCounter", s(result.judges_counter));
+
+                let roomCounter = 0;
+                let importDraws = result.draws.map((draw: any) => {
+                    let newDraw = {
+                        ...draw,
+                        roomsOne: draw.pairings_one.map((pairing: any) => {
+                            let newRoom = {
+                                ...pairing,
+                                roomID: roomCounter++,
+                                name: pairing.room
+                            }
+                            delete newRoom.room
+                            return newRoom
+                        }),
+                        roomsTwo: draw.pairings_two.map((pairing: any) => {
+                            let newRoom = {
+                                ...pairing,
+                                roomID: roomCounter++,
+                                name: pairing.room
+                            }
+                            delete newRoom.room
+                            return newRoom
+                        })
+                    }
+                    delete newDraw.pairings_one
+                    delete newDraw.pairings_two
+                    return newDraw
+                });
+                localStorage.setItem("draws", s(importDraws));
+                localStorage.setItem("roomCounter", s(roomCounter));
+
+            // CURRENT EXPORTS
+            } else {
+                localStorage.setItem("tournamentName", s(result.tournamentName));
+                localStorage.setItem("config", s(result.config));
+                localStorage.setItem("speakersOne", s(result.speakersOne));
+                localStorage.setItem("teamsOne", s(result.teamsOne));
+                localStorage.setItem("speakersTwo", s(result.speakersTwo));
+                localStorage.setItem("teamsTwo", s(result.teamsTwo));
+                localStorage.setItem("speakerCounter", s(result.speakerCounter));
+                localStorage.setItem("teamCounter", s(result.teamCounter));
+                localStorage.setItem("judges", s(result.judges));
+                localStorage.setItem("judgeCounter", s(result.judgeCounter));
+                localStorage.setItem("draws", s(result.draws));
+                localStorage.setItem("roomCounter", s(result.roomCounter));
+            }
+
         }
         fr.readAsText(files.item(0) as File);
 
