307d121776d7f39fc61cd89bd316e52381b31c70
rayoverweij <rayo.verweij@live.nl>
2020-07-31 20:55:10 +0200

    Toggle component

--- src/judges/JudgeRow.tsx
+++ src/judges/JudgeRow.tsx
@@ -1,8 +1,8 @@
-import React, { ChangeEvent } from 'react';
+import React from 'react';
 import { EditText } from '../utils/EditText';
+import { Toggle } from '../utils/Toggle';
 import { TrashButton } from '../utils/TrashButton';
 import { Judge } from '../types/Judge';
-import Form from 'react-bootstrap/Form';
 
 
 type JudgeRowProps = {
@@ -11,45 +11,17 @@ type JudgeRowProps = {
     deleteJudge: (judge: Judge) => void
 }
 
-type JudgeRowState = {
-    canChair: boolean,
-    atRound1: boolean,
-    atRound2: boolean,
-    atRound3: boolean,
-    [key: string]: boolean
-}
-
-class JudgeRow extends React.PureComponent<JudgeRowProps, JudgeRowState> {
+class JudgeRow extends React.PureComponent<JudgeRowProps> {
     constructor(props: JudgeRowProps) {
         super(props);
 
-        this.state = {
-            canChair: this.props.judge.canChair,
-            atRound1: this.props.judge.atRound1,
-            atRound2: this.props.judge.atRound2,
-            atRound3: this.props.judge.atRound3
-        }
-
         this.handleJudgeUpdate = this.handleJudgeUpdate.bind(this);
-        this.handleJudgeToggle = this.handleJudgeToggle.bind(this);
     }
 
-
-    handleJudgeUpdate(name: string, value: string) {
+    handleJudgeUpdate(name: string, value: string | boolean) {
         const judge = {...this.props.judge, [name]: value};
         this.props.updateJudge(judge);
     }
-
-    handleJudgeToggle(event: ChangeEvent<HTMLInputElement>) {
-        const {name, checked} = event.target;
-        const judge = {...this.props.judge};
-
-        judge[name] = checked;
-        this.setState({[name]: checked});
-
-        this.props.updateJudge(judge);
-    }
-
     
     render() {
         const judge = this.props.judge;
@@ -69,44 +41,32 @@ class JudgeRow extends React.PureComponent<JudgeRowProps, JudgeRowState> {
                         fn={this.handleJudgeUpdate} />
                 </td>
                 <td className="judge-table-toggle cell-low-padding">
-                    <Form.Check
-                        id={`judge-canchair-${judge.judgeID}`}
-                        type="switch"
+                    <Toggle
+                        id={judge.judgeID}
                         name="canChair"
-                        label=""
-                        onChange={this.handleJudgeToggle}
-                        checked={this.state.canChair}
-                        className={this.state.canChair ? "on" : "off"} />
+                        init={judge.canChair}
+                        fn={this.handleJudgeUpdate} />
                 </td>
                 <td className="judge-table-toggle cell-low-padding">
-                    <Form.Check
-                        id={`judge-r1-${judge.judgeID}`}
-                        type="switch"
+                    <Toggle
+                        id={judge.judgeID}
                         name="atRound1"
-                        label=""
-                        onChange={this.handleJudgeToggle}
-                        checked={this.state.atRound1}
-                        className={this.state.atRound1 ? "on" : "off"} />
+                        init={judge.atRound1}
+                        fn={this.handleJudgeUpdate} />
                 </td>
                 <td className="judge-table-toggle cell-low-padding">
-                    <Form.Check
-                        id={`judge-r2-${judge.judgeID}`}
-                        type="switch"
+                    <Toggle
+                        id={judge.judgeID}
                         name="atRound2"
-                        label=""
-                        onChange={this.handleJudgeToggle}
-                        checked={this.state.atRound2}
-                        className={this.state.atRound2 ? "on" : "off"} />
+                        init={judge.atRound2}
+                        fn={this.handleJudgeUpdate} />
                 </td>
                 <td className="judge-table-toggle cell-low-padding">
-                    <Form.Check
-                        id={`judge-r3-${judge.judgeID}`}
-                        type="switch"
+                    <Toggle
+                        id={judge.judgeID}
                         name="atRound3"
-                        label=""
-                        onChange={this.handleJudgeToggle}
-                        checked={this.state.atRound3}
-                        className={this.state.atRound3 ? "on" : "off"} />
+                        init={judge.atRound3}
+                        fn={this.handleJudgeUpdate} />
                 </td>
                 <td className="table-delete">
                     <div title={`Remove ${judge.name}`}
