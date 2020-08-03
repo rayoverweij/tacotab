f6c3a02625fd65d6086bd600b023f570c5a8bdd3
rayoverweij <rayo.verweij@live.nl>
2020-08-03 12:33:58 +0200

    Prelim update app message work

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 serviceWorker.register();
+
+// serviceWorker.register({
+//     onUpdate: (registration: ServiceWorkerRegistration) => {
+//         document.getElementById("alert-new-version-available")?.classList.add("show");
+//         if( registration && registration.waiting ) {
+//             registration.waiting.postMessage({type: 'SKIP_WAITING '});
+//         }
+//         window.location.reload(true);
+//     }
+// });
