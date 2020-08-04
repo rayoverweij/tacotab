b561ccf7487428a9ce9a4bcba3103d59d431526b
rayoverweij <rayo.verweij@live.nl>
2020-08-04 13:39:29 +0200

    Updates

---
+++ precache-manifest.cd8ce37daab8bb85cf0430a2cadcfe74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9705cf81e8839b76af150ba1f92ad8ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1d4516b967a14460f5b",
+    "url": "./static/css/main.cac548aa.chunk.css"
+  },
+  {
+    "revision": "c6ac6a2ac12a05e294e1",
+    "url": "./static/js/2.b039e60e.chunk.js"
+  },
+  {
+    "revision": "226cb6491056a6a9a4dd6fe6cc73cac9",
+    "url": "./static/js/2.b039e60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d4516b967a14460f5b",
+    "url": "./static/js/main.64afc56f.chunk.js"
+  },
+  {
+    "revision": "2c4bc254f53da9b2f6cd",
+    "url": "./static/js/runtime-main.c2d6a2f0.js"
+  },
+  {
+    "revision": "026cb8cd1d99d846a457f6729d59d0ca",
+    "url": "./static/media/icon-github.026cb8cd.svg"
+  },
+  {
+    "revision": "15863a1a6651536e12ee814becfae8b4",
+    "url": "./static/media/logo.15863a1a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tacotab/precache-manifest.a7db72223a7296d89940f30acf21592d.js"
+  "./precache-manifest.cd8ce37daab8bb85cf0430a2cadcfe74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tacotab/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
